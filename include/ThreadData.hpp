5a8d3ad17c23befcf77ceadcfcff4f67b2fbe178
MrShip15 <mr_ship2001@mail.ru>
2021-01-20 13:23:29 +0300

    fin

---
+++ include/ThreadData.hpp
@@ -0,0 +1,16 @@
+
+#ifndef INCLUDE_THREADDATA_HPP_
+#define INCLUDE_THREADDATA_HPP_
+
+#include <boost/process.hpp>
+#include <iostream>
+#include <random>
+
+#include "Parameters.hpp"
+struct ThreadData {
+  ThreadData() = delete;
+
+  bool isTerminated = false;
+  boost::process::child currentProcess;
+};
+#endif  // INCLUDE_THREADDATA_HPP_
